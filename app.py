f1fc04a3053e4a737b9a3a476ce874a3b419c9ae
enes-kizilaslan <117919509+enes-kizilaslan@users.noreply.github.com>
2025-04-26 03:28:41 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,6 +18,17 @@ import os
 import zipfile
 from utils import load_models, load_feature_lists, load_model_performances, prepare_input_data, make_predictions
 
+# Model listesi
+MODEL_LIST = [
+    'AdaBoost_7_Sosyal', 'LogisticRegression_7_Sosyal', 'RandomForest_7_Sosyal', 'XGBoost_7_Sosyal',
+    'AdaBoost_7_Dil', 'LogisticRegression_7_Dil', 'RandomForest_7_Dil', 'XGBoost_7_Dil',
+    'AdaBoost_7_Motor', 'LogisticRegression_7_Motor', 'RandomForest_7_Motor', 'XGBoost_7_Motor',
+    'AdaBoost_7_OSB', 'LogisticRegression_7_OSB', 'RandomForest_7_OSB', 'XGBoost_7_OSB',
+    'AdaBoost_7_DEHB', 'LogisticRegression_7_DEHB', 'RandomForest_7_DEHB', 'XGBoost_7_DEHB',
+    'AdaBoost_7_Ozgul', 'LogisticRegression_7_Ozgul', 'RandomForest_7_Ozgul', 'XGBoost_7_Ozgul',
+    'AdaBoost_7_Zihinsel', 'LogisticRegression_7_Zihinsel', 'RandomForest_7_Zihinsel', 'XGBoost_7_Zihinsel'
+]
+
 # Sayfa yapılandırması
 st.set_page_config(
     page_title="Nörogelişimsel Bozukluk Tarama Sistemi",
